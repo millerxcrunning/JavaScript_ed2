31cf917660f90bb22121b205a7b26da69fd864db
millerxcrunning <millerxcrunning@hotmail.com>
2020-08-13 13:38:17 -0400

    starting on leetcode problem balancedStringSplit

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -635,3 +635,14 @@ var createTargetArray = function(nums, index) {
 };
 
 console.log(createTargetArray([0,1,2,3,4], [0,1,2,2,1]));
+
+/**
+ * Balanced strings are those who have equal quantity of 'L' and 'R' characters. 
+ * Given a balanced string s split it in the maximum amount of balanced strings. 
+ * Return the maximum amount of splitted balanced strings.
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+    
+};
